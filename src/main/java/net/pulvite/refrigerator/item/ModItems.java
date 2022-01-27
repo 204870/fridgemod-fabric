c86b5b93841458df4464a77d21222f23d478e8c6
Eliana <47612469+204870@users.noreply.github.com>
2022-01-27 14:59:53 -0500

    mod items time

---
+++ src/main/java/net/pulvite/refrigerator/item/ModItems.java
@@ -0,0 +1,10 @@
+package net.pulvite.refrigerator.item;
+
+import net.pulvite.refrigerator.refrigeratormod
+
+public class ModItems {
+    public static void registerModItems() {
+        refrigeratormod.LOGGER.info("Registering Mod Items for Refrigerator Mod")
+    }
+
+}
