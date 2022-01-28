be7bc5a7439366bd1e6820d122068282b87189c2
Eliana <47612469+204870@users.noreply.github.com>
2022-01-28 00:57:20 -0500

    Update refrigeratormod.java

--- src/main/java/net/pulvite/refrigerator/refrigeratormod.java
+++ src/main/java/net/pulvite/refrigerator/refrigeratormod.java
@@ -8,7 +8,8 @@ public class refrigeratormod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final String MOD_ID = "refrigeratormod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("refrigeratormod");
 
 	@Override
 	public void onInitialize() {
