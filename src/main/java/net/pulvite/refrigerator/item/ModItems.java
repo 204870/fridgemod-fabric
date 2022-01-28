3910ac3cddd4eea8aa017bb3017f1d0febdb590e
Eliana <47612469+204870@users.noreply.github.com>
2022-01-27 22:34:06 -0500

    fkcn semicolons

--- src/main/java/net/pulvite/refrigerator/item/ModItems.java
+++ src/main/java/net/pulvite/refrigerator/item/ModItems.java
@@ -1,10 +1,10 @@
 package net.pulvite.refrigerator.item;
 
-import net.pulvite.refrigerator.refrigeratormod
+import net.pulvite.refrigerator.refrigeratormod;
 
 public class ModItems {
     public static void registerModItems() {
-        refrigeratormod.LOGGER.info("Registering Mod Items for Refrigerator Mod")
+        refrigeratormod.LOGGER.info("Registering Mod Items for refrigeratormod");
     }
 
 }
