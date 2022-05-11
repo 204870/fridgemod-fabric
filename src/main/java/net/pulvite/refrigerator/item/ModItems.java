40f112e10d023b516c2d9d7ef6a9c73daa6cfaad
Eliana <47612469+204870@users.noreply.github.com>
2022-05-11 15:45:46 -0700

    added frozen sword and frozen pickaxe

--- src/main/java/net/pulvite/refrigerator/item/ModItems.java
+++ src/main/java/net/pulvite/refrigerator/item/ModItems.java
@@ -4,17 +4,31 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.MusicDiscItem;
+import net.minecraft.item.SwordItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.pulvite.refrigerator.item.custom.ModMusicDiscItem;
+import net.pulvite.refrigerator.item.custom.ModPickaxeItem;
 import net.pulvite.refrigerator.refrigeratormod;
 import net.pulvite.refrigerator.sound.ModSounds;
 
 public class ModItems {
-    public static final Item REFRIGERATOR_DOOR = registerItem("refrigerator_door", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
+    //test stuffs
+    public static final Item REFRIGERATOR_DOOR = registerItem("refrigerator_door", new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+
+    //tools
+    public static final Item FROZEN_SWORD = registerItem("frozen_sword", new SwordItem(ModToolMaterials.ICE,2, 1f,
+            new FabricItemSettings().group(ItemGroup.COMBAT)));
+
+    public static final Item FROZEN_PICKAXE = registerItem("frozen_pickaxe", new ModPickaxeItem(ModToolMaterials.ICE,1, 0f,
+            new FabricItemSettings().group(ItemGroup.TOOLS)));
+
+    //music discs
     public static final Item WATCH_OUT_MUSIC_DISC = registerItem("watch_out_music_disc", new ModMusicDiscItem(7, ModSounds.WATCH_OUT,
             new FabricItemSettings().group(ItemGroup.MISC).maxCount(1)));
+
+
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(refrigeratormod.MOD_ID, name), item);
---
+++ src/main/java/net/pulvite/refrigerator/item/ModToolMaterials.java
@@ -0,0 +1,70 @@
+package net.pulvite.refrigerator.item;
+
+import net.fabricmc.yarn.constants.MiningLevels;
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.tag.ItemTags;
+import net.minecraft.util.Lazy;
+
+import java.util.function.Supplier;
+
+public enum ModToolMaterials implements ToolMaterial {
+/*
+Reference tool materials
+    WOOD(MiningLevels.WOOD, 59, 2.0f, 0.0f, 15, () -> Ingredient.fromTag(ItemTags.PLANKS)),
+    STONE(MiningLevels.STONE, 131, 4.0f, 1.0f, 5, () -> Ingredient.fromTag(ItemTags.STONE_TOOL_MATERIALS)),
+    IRON(MiningLevels.IRON, 250, 6.0f, 2.0f, 14, () -> Ingredient.ofItems(Items.IRON_INGOT)),
+    DIAMOND(MiningLevels.DIAMOND, 1561, 8.0f, 3.0f, 10, () -> Ingredient.ofItems(Items.DIAMOND)),
+    GOLD(MiningLevels.WOOD, 32, 12.0f, 0.0f, 22, () -> Ingredient.ofItems(Items.GOLD_INGOT)),
+    NETHERITE(MiningLevels.NETHERITE, 2031, 9.0f, 4.0f, 15, () -> Ingredient.ofItems(Items.NETHERITE_INGOT));
+
+ */
+    ICE(MiningLevels.DIAMOND, 48, 10.0f, 1.0f, 20, () -> Ingredient.ofItems(Items.BLUE_ICE));
+
+    private final int miningLevel;
+    private final int itemDurability;
+    private final float miningSpeed;
+    private final float attackDamage;
+    private final int enchantability;
+    private final Lazy<Ingredient> repairIngredient;
+
+    private ModToolMaterials(int miningLevel, int itemDurability, float miningSpeed, float attackDamage, int enchantability, Supplier<Ingredient> repairIngredient) {
+        this.miningLevel = miningLevel;
+        this.itemDurability = itemDurability;
+        this.miningSpeed = miningSpeed;
+        this.attackDamage = attackDamage;
+        this.enchantability = enchantability;
+        this.repairIngredient = new Lazy<Ingredient>(repairIngredient);
+    }
+
+    @Override
+    public int getDurability() {
+        return this.itemDurability;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return this.miningSpeed;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return this.miningLevel;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return this.repairIngredient.get();
+    }
+}
